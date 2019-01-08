2900ae592807f04881c09b7aecfcc9ad89719a27
Kmosiej <kmosiej@isolution.pl>
2019-01-08 22:18:12 +0100

    DAY2 PART2 Implementacja metod aż wszystkie testy będą zielone.

--- src/main/java/pl/advent/Day2.java
+++ src/main/java/pl/advent/Day2.java
@@ -1,5 +1,8 @@
 package pl.advent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author kmosiej
@@ -9,11 +12,30 @@ public class Day2 implements ActivistDay2 {
 
     @Override
     public boolean checkForTwos(String row) {
-        return false;
+        Map<Character, Long> map = count(row);
+
+        return map.values().contains(2L);
     }
 
     @Override
     public boolean checkForThrees(String row) {
-        return false;
+        Map<Character, Long> map = count(row);
+
+        return map.values().contains(3L);
+    }
+
+    private Map<Character, Long> count(String row) {
+        Map<Character, Long> map = new HashMap<>();
+
+        char[] chars = row.toCharArray();
+
+        for (char aChar : chars) {
+            if (map.keySet().contains(aChar)) {
+                map.put(aChar, map.get(aChar) + 1);
+            } else {
+                map.put(aChar, 1L);
+            }
+        }
+        return map;
     }
 }
